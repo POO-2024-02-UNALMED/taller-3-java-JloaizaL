6773675365b3135f1c4d307062fec67146caf6d7
JloaizaL <jloaizal@unal.edu.co>
2024-11-18 19:20:59 -0500

    prueba

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,103 +1,82 @@
 package taller3.televisores;
 
 public class TV {
-    private Marca marca;
-    private int canal = 1;
-    private int precio = 500;
-    private boolean estado;
-    private int volumen = 1;
-    private Control control;
-    private static int numTV;
-    public static void main(String[] args) {
+	private Marca marca;
+	private int canal;
+	private int precio;
+	private boolean estado;
+	private int volumen;
+	private Control control;
+	private static int numTV = 0;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+		this.canal = 1;
+		this.volumen = 1;
+		this.precio = 500;
+		TV.numTV++;
+	}
 
-    }
+	public static int getNumTV() {
+		return TV.numTV;
+	}
+	public static void setNumTV(int numTV) {
+		TV.numTV = numTV;
+	}
+	public void turnOn() {
+		this.estado = true;
+	}
+	public void turnOff() {
+		this.estado = false;
+	}
+	public void canalUp() {
+		this.setCanal(this.canal + 1);
+	}
+	public void canalDown() {
+		this.setCanal(this.canal - 1);
+	}
+	public void volumenUp() {
+		this.setVolumen(this.volumen + 1);
+	}
+	public void volumenDown() {
+		this.setVolumen(this.volumen - 1);
+	}
 
-    public TV (Marca marca, boolean estado){
-        this.estado = estado;
-        this.setMarca(marca);
-        this.setNumTV(TV.numTV + 1);
-    }
+	public Marca getMarca() {
+		return this.marca;
+	}
+	public int getCanal() {
+		return this.canal;
+	}
+	public int getPrecio() {
+		return this.precio;
+	}
+	public int getVolumen() {
+		return this.volumen;
+	}
+	public Control getControl() {
+		return this.control;
+	}
+	public boolean getEstado() {
+		return this.estado;
+	}
 
-    public Marca getMarca(){
-        return this.marca;
-    }
-    public int getCanal(){
-        return this.canal;
-    }
-    public int getPrecio(){
-        return this.precio;
-    }
-    public int getVolumen(){
-        return this.volumen;
-    }
-    public Control getControl(){
-        return this.control;
-    }
-
-
-    public void setMarca(Marca marca){
-        this.marca = marca;
-    }
-    public void setCanal(int canal){
-        this.canal = canal;
-    }
-    public void setPrecio(int precio){
-        this.precio = precio;
-    }
-    public void setVolumen(int volumen){
-        this.volumen = volumen;
-    }
-    public void setControl(Control control){
-        this.control = control;
-    }
-
-    public int getNumTV(){
-        return TV.numTV;
-    }
-    public void setNumTV(int numTV){
-        TV.numTV = numTV;
-    }
-
-    public void turnOff(){
-        this.estado = false;
-    }
-    public void turnOn(){
-        this.estado = true;
-    }
-
-    public boolean getEstado(){
-        return estado;
-    }
-
-    public void canalUp(){
-        if(this.estado){
-            if(this.canal >= 1 && this.canal < 120){
-                this.setCanal(this.canal + 1);
-            }
-        }
-    }
-
-    public void canalDown(){
-        if(this.estado){
-            if(this.canal > 1 && this.canal <= 120){
-                this.setCanal(this.canal - 1);
-            }
-        }
-    }
-
-    public void volumenUp(){
-        if(this.estado){
-            if(this.volumen >= 0 && this.volumen < 7){
-                this.setVolumen(this.volumen + 1);
-            }
-        }
-    }
-
-    public void volumenDown(){
-        if(this.estado){
-            if(this.volumen > 0 && this.volumen <= 7){
-                this.setVolumen(this.volumen - 1);
-            }
-        }
-    }
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	public void setCanal(int canal) {
+		if (this.estado && canal >= 1 && canal <= 120)
+			this.canal = canal;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public void setVolumen(int volumen) {
+		if (this.estado && volumen >= 0 && volumen <= 7)
+			this.volumen = volumen;
+	}
+	public void setControl(Control control) {
+		this.control = control;
+	}
 }
